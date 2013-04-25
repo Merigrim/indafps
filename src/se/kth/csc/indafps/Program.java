3f36b77d4eae13f4ab5f4fed55bc31c66d0652fd
Merigrim <marcusostergren@gmail.com>
2013-04-25 16:37:17 +0200

    Initial commit. Set up the Eclipse project and added the entry point class.

---
+++ src/se/kth/csc/indafps/Program.java
@@ -0,0 +1,19 @@
+package se.kth.csc.indafps;
+
+/**
+ * Entry point for the game.
+ * 
+ * @author Marcus Åbrandt Östergren
+ * @author Oscar Friberg
+ * @version 2013-04-25
+ */
+public class Program {
+    /**
+     * The main function. This is called when the game starts up and is
+     * responsible for initializing the engine object.
+     * @param args The command line arguments passed to the program
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
